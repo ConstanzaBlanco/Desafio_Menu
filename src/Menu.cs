6a87b2a44cc9bf197c5aa8ff93e812b8d52f1a16
ManuelNey <mnet.cabrera@correo.ucu.edu.uy>
2024-08-21 11:39:03 -0300

    Primer intento, se agragan, borran y comproboran los platillos

--- src/Menu.cs
+++ src/Menu.cs
@@ -9,4 +9,18 @@ using System.Collections;
 public class Menu
 {
     private ArrayList dishes = new ArrayList();
+    public string AddDish(object platillo)
+    {
+        Dish.dishes.add(platillo)
+    }
+    public string RemoveDish(object platillo)
+    {
+        Dish.dishes.Remove(platillo)
+    }
+
+    public bool GetDishByName(string platillo)
+    {
+        Dish.dishes.Contains(platillo)
+    }
+    
 }
