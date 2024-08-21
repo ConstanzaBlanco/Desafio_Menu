b722df55255175ba86ba52b0723279eb1faf464e
ConstanzaBlanco <constanza.blanco@correo.ucu.edu.uy>
2024-08-21 11:40:21 -0300

    Hecho

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,9 +1,35 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+
 namespace Ucu.Poo.Restaurant;
 
 /// <summary>
 /// Representa un platillo individual en el menú del restaurante.
 /// </summary>
-public class Dish
+public class Dish 
 {
-    
+    private string Name
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+
+    private string Price
+    {
+        get { return Price; }
+        set { Price = value; }
+    }
+
+    private string IsVegetarian
+    {
+        get { return IsVegetarian; }
+        set { IsVegetarian = value; }
+    }
+
+    public Dish(string name, string price, string isvegetarian)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.IsVegetarian = isvegetarian;
+    }
 }
