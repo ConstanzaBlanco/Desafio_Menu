8e24747a362abf39ce74412026f03beae178ce02
Your Name <you@example.com>
2024-08-21 11:39:39 -0300

    Incluyo algunas de las responsabilidades(isoccupied,number,order)

--- src/Table.cs
+++ src/Table.cs
@@ -7,7 +7,33 @@ using System.Collections;
 /// </summary>
 public class Table
 {
+    private int number;
+
+    public int Number
+    {
+        get { return this.number;}
+        set { number = value; }
+    }
+
+    private bool isoccupied;
+
+    public bool IsOcuppied
+    {
+        get { return this.isoccupied;}
+        set { isoccupied = value; }
+    }
     private ArrayList order = new ArrayList();
+
+    public bool Occupy()
+    {
+        return 
+    }
+    
+    
+    
+    
+    
+    
     public bool HasOrders()
     {
         return this.order.Count > 0;
