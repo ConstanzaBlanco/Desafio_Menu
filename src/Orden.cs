e53ceca78a82f8ea6b506b51d19383bc2884a97f
ManuelNey <mney.cabrera@correo.ucu.edu.uy>
2024-08-27 20:52:57 -0300

    Clase orden

--- src/Orden.cs
+++ src/Orden.cs
@@ -1,6 +1,24 @@
+using System.Runtime.InteropServices.JavaScript;
 namespace Ucu.Poo.Restaurant;
+using System.Collections;
 
-public class Orden
+public class Order
 {
-    
+    public ArrayList Platos { get; set; } = new ArrayList();
+
+    public double GetTotal()
+    {
+        double total = 0;
+        foreach (Dish plato in this.Platos)
+        {
+            total += plato.Price;
+        }
+
+        return total;
+    }
+
+    public void AddToOrder(Dish dish)
+    {
+        this.Platos.Add(dish);
+    }
 }
